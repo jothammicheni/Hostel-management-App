3e5c6f29f4f232d30582081cb788e117772652fe
jothammicheni <jothammurimi2001@gmail.com>
2023-11-10 22:44:13 +0300

    registering the user

--- app/src/main/java/com/example/sunrisehostelms/UserLoginActivity.java
+++ app/src/main/java/com/example/sunrisehostelms/UserLoginActivity.java
@@ -11,7 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.sunrisehostelms.Core.DBHelper;
 import com.example.sunrisehostelms.Core.PersonalDetailsContract;
 
